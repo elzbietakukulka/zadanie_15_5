899feaf7705b2d2789df054e45dd799759a6e71a
Elżbieta Kukułka <ellakaras@gmail.com>
2018-04-11 17:10:16 +0200

    Changes at App.js

--- components/App.js
+++ components/App.js
@@ -13,35 +13,63 @@ App = React.createClass({
         },
 
         handleSearch: function(searchingText) {  
+            
             this.setState({
               loading: true  
             });
-            this.getGif(searchingText, function(gif) {  
-              this.setState({  
-                loading: false,  
-                gif: gif,  
-                searchingText: searchingText  
-              });
-            }.bind(this));
-          },
+            
+            var self = this;
 
-        getGif: function(searchingText, callback) {  
+            this.getGif(searchingText)
+
+                .then(function(gif) {  
                 
+                    self.setState({  
+                        loading: false,  
+                        gif: gif,  
+                        searchingText: searchingText  
+                      });
+            })
+
+            .catch(function(error) {
+                console.log(error);
+            });
+        },
+
+        
+        getGif: function(searchingText) {  
+                
+            return new Promise (
+            function(resolve, reject) {
+
                 var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
-                var xhr = new XMLHttpRequest();  
+                var xhr = new XMLHttpRequest();
+
                 xhr.open('GET', url);
                 xhr.onload = function() {
+                    
                     if (xhr.status === 200) {
+                       
                        var data = JSON.parse(xhr.responseText).data; 
                         var gif = {  
                             url: data.fixed_width_downsampled_url,
                             sourceUrl: data.url
                         };
-                        callback(gif);  
+                        resolve(gif);  
+                    
+                    } else {
+                        reject(new Error(this.statusText));
                     }
+                    
                 };
+
                 xhr.send();
-            },
+                
+            });
+        },
+                  
+                
+                
 
         render: function(){
         var styles = {
---
+++ script-compiled.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var element = React.createElement(App);
+
+ReactDOM.render(element, document.getElementById('app'));
